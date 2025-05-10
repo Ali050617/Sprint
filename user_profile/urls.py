161fe64661ce66162383c50a93bc6844f4c5ae20
Samandar <samandarnayimjonov@gmail.com>
2025-05-10 13:41:22 +0500

    token/refresh logika tuliq bitdi

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('auth/register/', views.UserRegisterViews.as_view(), name='register'),
     path('auth/login/', views.UserLoginView.as_view(), name='login'),
     path('auth/verify-email/', views.VerifyEmailView.as_view(), name='verify-email'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
