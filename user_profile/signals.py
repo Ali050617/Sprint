cc4bcfa5b5decfff46d02ad7b23949c7bf259f77
Samandar <samandarnayimjonov@gmail.com>
2025-05-11 17:37:58 +0500

    updated

--- user_profile/signals.py
+++ user_profile/signals.py
@@ -1,9 +1,36 @@
+import os
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db.models.signals import post_delete, pre_save
 from .models import User, UserProfile
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
+
+@receiver(post_delete, sender=UserProfile)
+def delete_user_image_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+
+@receiver(pre_save, sender=UserProfile)
+def delete_old_image_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    try:
+        old_instance = UserProfile.objects.get(pk=instance.pk)
+    except UserProfile.DoesNotExist:
+        return
+
+    old_image = old_instance.image
+    new_image = instance.image
+
+    if old_image and old_image != new_image:
+        if os.path.isfile(old_image.path):
+            os.remove(old_image.path)
