fcf0dc635bca814e44edba4d8834c6fb7e6d381f
Samandar <samandarnayimjonov@gmail.com>
2025-05-12 13:48:54 +0500

    setings tugrlandi DRY tamoyiliga roya qlindi

--- config/settings.py
+++ config/settings.py
@@ -131,20 +131,18 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
-
 
 
 AUTH_USER_MODEL = 'user_profile.User'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
-}
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
