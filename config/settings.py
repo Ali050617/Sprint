7d1b601d7346a7b29616c10d2e036a9da11f9d6f
Samandar <samandarnayimjonov@gmail.com>
2025-05-13 19:15:46 +0500

    updated

--- config/settings.py
+++ config/settings.py
@@ -123,12 +123,3 @@ LOGGING = {
         },
     },
 }
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'samandarnayimjonov@gmail.com'
-EMAIL_HOST_PASSWORD = 'clcqdwbnklubwnsq'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
