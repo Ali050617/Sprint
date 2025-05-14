44d2512bc17ff079f6589799442a35dc649d98cd
Samandar <samandarnayimjonov@gmail.com>
2025-05-14 06:37:15 +0500

    image delete boganda upload bolgan papkadan ham delete bolishiga signal yozildi

--- user_profile/signals.py
+++ user_profile/signals.py
@@ -9,12 +9,13 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 
+# AUTO CREATE USER PROFILE
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
-
+# VERIFY EMAIL
 @receiver(post_save, sender=User)
 def send_verification_email_on_create(sender, instance, created, **kwargs):
     if created and not instance.is_verified:
@@ -34,27 +35,22 @@ def send_verification_email_on_create(sender, instance, created, **kwargs):
             fail_silently=False,
         )
 
+# IMAGE UPDATE
+@receiver(pre_save, sender=UserProfile)
+def delete_old_image_on_update(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_instance = UserProfile.objects.get(pk=instance.pk)
 
+            if old_instance.image and (not instance.image or old_instance.image != instance.image):
+                if os.path.isfile(old_instance.image.path):
+                    os.remove(old_instance.image.path)
+        except UserProfile.DoesNotExist:
+            pass
+
+# IMAGE DELETE
 @receiver(post_delete, sender=UserProfile)
-def delete_user_image_on_delete(sender, instance, **kwargs):
+def delete_image_on_profile_delete(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
-
-
-@receiver(pre_save, sender=UserProfile)
-def delete_old_image_on_change(sender, instance, **kwargs):
-    if not instance.pk:
-        return
-
-    try:
-        old_instance = UserProfile.objects.get(pk=instance.pk)
-    except UserProfile.DoesNotExist:
-        return
-
-    old_image = old_instance.image
-    new_image = instance.image
-
-    if old_image and old_image != new_image:
-        if os.path.isfile(old_image.path):
-            os.remove(old_image.path)
