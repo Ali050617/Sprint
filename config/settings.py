a856297d52b284ce3eda346b3e011932dc917e7f
Muhammadali <muhammadalikadyrov06@gmail.com>
2025-05-16 18:29:15 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'notifications.apps.NotificationsConfig',
